f5faa78ceeaf56cc87f7f6cd5f17b5c76544a6ca
Steven Ross <ross.steven@me.com>
2020-04-12 15:18:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -7,7 +7,7 @@ while row_index < src.length do
   element_index = 0
   while element_index < src[row_index].length do
     if src[row_index][element_index].class == String
-      final_values += src[row_index][element_index].join(" ")
+      final_values += src[row_index][element_index] + " "
       end
     element_index += 1
     end
